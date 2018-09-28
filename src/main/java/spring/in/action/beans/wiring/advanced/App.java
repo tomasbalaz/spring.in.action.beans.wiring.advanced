e16de55718e0fb3e3794cd3c983eb90e9759efd2
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-09-28 13:25:29 +0200

    Application created

---
+++ src/main/java/spring/in/action/beans/wiring/advanced/App.java
@@ -0,0 +1,13 @@
+package spring.in.action.beans.wiring.advanced;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
