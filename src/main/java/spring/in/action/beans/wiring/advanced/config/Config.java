d86ddd16f48cf3abe72f00cd642ad25dc6edf07f
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-09-28 13:38:19 +0200

    Create config class

--- src/main/java/spring/in/action/beans/wiring/advanced/config/Config.java
+++ src/main/java/spring/in/action/beans/wiring/advanced/config/Config.java
@@ -1,4 +1,4 @@
-package spring.in.action.beans.wiring.advanced;
+package spring.in.action.beans.wiring.advanced.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
